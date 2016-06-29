5b23cb3631db049f39003d39cdb5073641e63b77
Oleg Girko <ol@infoserver.lv>
2016-06-29 23:06:59 +0100

    [api] Autodetect mysqld pathname for api test.
    Different distros have mysqld binary in different places.
    This change makes api test to search for this binary in
    "/usr/bin", "/usr/sbin" and "/usr/libexec" directories.
    
    Signed-off-by: Oleg Girko <ol@infoserver.lv>

--- src/api/script/prepare_spec_tests.sh
+++ src/api/script/prepare_spec_tests.sh
@@ -8,6 +8,18 @@ MYSQL_DATADIR=$MYSQL_BASEDIR/data
 MYSQL_SOCKET_DIR=`mktemp -d`
 MYSQL_SOCKET=$MYSQL_SOCKET_DIR/mysql.socket
 
+MYSQL_SERVER=
+for dir in /usr/bin /usr/sbin /usr/libexec; do
+  if [ -x "$dir/mysqld" ]; then
+    MYSQL_SERVER="$dir/mysqld"
+    break
+  fi
+done
+if [ -z "$MYSQL_SERVER" ]; then
+  echo mysqld not found >&2
+  exit 1
+fi
+
 MYSQLD_USER=`whoami`
 if [[ $EUID == 0 ]];then
   MYSQLD_USER=mysql
@@ -19,7 +31,7 @@ rm -rf $MYSQL_DATADIR $MYSQL_SOCKET
 mkdir -p $MYSQL_BASEDIR
 chown -R $MYSQLD_USER $MYSQL_BASEDIR
 mysql_install_db --user=$MYSQLD_USER --datadir=$MYSQL_DATADIR
-/usr/sbin/mysqld --user=$MYSQLD_USER --datadir=$MYSQL_DATADIR --skip-networking --socket=$MYSQL_SOCKET &
+$MYSQL_SERVER --user=$MYSQLD_USER --datadir=$MYSQL_DATADIR --skip-networking --socket=$MYSQL_SOCKET &
 sleep 2
 
 ##################### api
